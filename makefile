5f5f4062a4a0c319fd34a366cdfeca9f3cbd7f73
michael-duy <doducduy.vietnam@gmail.com>
2022-04-14 22:54:11 +0700

    Update docker

--- makefile
+++ makefile
@@ -1,10 +1,14 @@
 up-build:
 	docker-compose up --build
+up:
+	docker-compose up
 down:
 	docker-compose down
+start:
+	docker-compose start
 stop:
 	docker-compose stop
 up-d:
 	docker-compose up -d
 log:
-	docker log -f -t
+	docker-compose logs -f -t
--- server/index.js
+++ server/index.js
@@ -1,24 +1,39 @@
-const io = require('socket.io')(3000);
+const io = require('socket.io');
+const express = require('express');
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const ioServer = io(server);
+server.listen(3000);
+
+console.log("demo 1");
+app.get("/", (request, response) => {
+  response.status(200).json({
+    message: "hello world",
+    data1: "Demo 1"
+  })
+});
+
 
 const getClientRoom = () => {
     let index = 0;
     while (true) {
-      if(!io.sockets.adapter.rooms[index] || io.sockets.adapter.rooms[index].length < 2) {
+      if(!ioServer.sockets.adapter.rooms[index] || ioServer.sockets.adapter.rooms[index].length < 2) {
         return index;
       }
       index ++;
     }
 }
 
-io.on('connect', socket => {
+ioServer.on('connect', socket => {
     const clientRoom = getClientRoom();
 
     socket.join(clientRoom);
 
-    if(io.sockets.adapter.rooms[clientRoom].length < 2) {
-        io.in(clientRoom).emit('statusRoom', 'Waiting for a stranger ...');
+    if(ioServer.sockets.adapter.rooms[clientRoom].length < 2) {
+        ioServer.in(clientRoom).emit('statusRoom', 'Waiting for a stranger ...');
     } else {
-        io.in(clientRoom).emit('statusRoom', 'Strangers have entered the room');
+        ioServer.in(clientRoom).emit('statusRoom', 'Strangers have entered the room');
     }
 
     socket.on('sendMessage', function (message) {
